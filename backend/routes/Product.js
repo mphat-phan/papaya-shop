f60e50c7903b54aa4d8059563895e345f14d08ee
Phu Nguyen <phun1204@gmail.com>
2022-11-02 22:22:30 +0700

    ok

--- backend/routes/Product.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getProducts, createProduct } from "../controllers/Product.js";
-
-const router = express.Router();
-
-router.route("/").get(getProducts).post(createProduct);
-
-export default router;
--- backend/routes/Review.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createReview } from "../controllers/Review.js";
-
-const router = express.Router();
-
-router.route("/").post(createReview);
-
-export default router;
