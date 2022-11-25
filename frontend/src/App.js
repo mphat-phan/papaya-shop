1fbd9a2b0dcf4603b81a5fb15c23719423cf8be9
mphat-phan <phanminhphat2001@gmail.com>
2022-11-25 22:19:53 +0700

    fix route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -56,7 +56,6 @@ const App = () => {
               />
               <Route path="/admin/product/:id" component={ProductEditScreen} />
             </Switch>
-            <Route path="/admin/productlist" component={ProductListScreen} />
             <Route path="/admin/categorylist" component={CategoryListScreen} />
             <Switch>
               <Route
@@ -77,16 +76,8 @@ const App = () => {
               component={DashboardProductScreen}
             />
             <Route path="/admin/productlist" component={ProductListScreen} />
-            <Route
-              path="/admin/dashboard-revenue"
-              component={DashboardRevenueScreen}
-            />
-            <Route
-              path="/admin/dashboard-product"
-              component={DashboardProductScreen}
-            />
+            
 
-            <Route path="/admin/productlist" component={ProductListScreen} />
             <Route path="/admin/user/:id" component={UserEditScreen} />
             <Route path="/admin/userlist" component={UserListScreen} />
             <Route path="/product/:id" component={ProductScreen} />
